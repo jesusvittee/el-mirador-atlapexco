093df97f690f016985495f9c0f5bf772889b45b8
jesusvite <nicolasvitejesus@gmail.com>
2023-05-31 13:30:54 -0600

    solo cambie posiciones del main

--- fotos/js/fotos.js
+++ fotos/js/fotos.js
@@ -12,13 +12,35 @@ botonSalir.addEventListener('click', addDesktopMenu);
 const fotoLista = [];
 
 fotoLista.push({
-    imagen:'https://scontent.fmex1-6.fna.fbcdn.net/v/t39.30808-6/265519918_471481564600247_2392190754075689668_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=k_kxEvO81tkAX_sq5sH&_nc_oc=AQkVdR21G6t2XF4iyl1GXn1nVTBYLpAc6C7skHYIyj1GEoQuetLaKWTA2mj4U0QaU4xcDUKthLX1TAjrjwMXOkqD&_nc_ht=scontent.fmex1-6.fna&oh=00_AfDWTdnZS1RZ8aqrhZ2f0oxsI8QTn7rdY64ev_PZzbBDcQ&oe=646AED9F',
+    imagen:'./img/img1.jpg',
 });
 fotoLista.push({
-    imagen:'https://scontent.fmex1-1.fna.fbcdn.net/v/t39.30808-6/288660762_394954082651364_2895095097902050495_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=uS46-jaPJisAX9lgL19&_nc_ht=scontent.fmex1-1.fna&oh=00_AfA4m6at5zi8rpLkU5CTTxrVDpieDumGzgpj7U3rFI7cXA&oe=646C014C',
+    imagen:'./img/img2.jpg',
 });
 fotoLista.push({
-    imagen:'https://scontent.fmex1-1.fna.fbcdn.net/v/t1.6435-9/101391397_103035574778183_1072789294743027712_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_ohc=zYTuWBijrxkAX-lY7t0&_nc_ht=scontent.fmex1-1.fna&oh=00_AfCYozOqfQCDL9lOrYhWmpaSl44kwkVBt3VGqIpI9mKhbw&oe=648F2E93',
+    imagen:'./img/img3.jpg',
+});
+fotoLista.push({
+    imagen:'./img/img4.jpg',
+});
+fotoLista.push({
+    imagen:'./img/img5.jpg',
+});
+
+fotoLista.push({
+    imagen:'./img/img6.jpg',
+});
+fotoLista.push({
+    imagen:'./img/img7.jpg',
+});
+fotoLista.push({
+    imagen:'./img/img8.jpg',
+});
+fotoLista.push({
+    imagen:'./img/img9.jpg',
+});
+fotoLista.push({
+    imagen:'./img/img10.jpg',
 });
 
 
